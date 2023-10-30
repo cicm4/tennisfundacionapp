5cd2d76f2c795029318aa266c63e516b038c0c4c
cicm4 <camilocorreall44@gmail.com>
2023-10-30 10:05:55 -0500

    second commit with basic route and theme files

---
+++ lib/theme.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+ThemeData generalTheme = ThemeData(
+  brightness: Brightness.dark,
+  textTheme: const TextTheme(
+    bodyLarge: TextStyle(fontSize: 18.0, color: Colors.white),
+    bodyMedium: TextStyle(fontSize: 16.0, color: Colors.white),
+    bodySmall: TextStyle(fontSize: 14.0 , color: Colors.white),
+    ),
+);
