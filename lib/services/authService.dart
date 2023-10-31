03122a01c04c7e242b334357bacb7ef3b9e1bc32
cicm4 <camilocorreall44@gmail.com>
2023-10-31 11:20:18 -0500

    Began writing AuthService and DBService classes

--- lib/services/authService.dart
+++ lib/services/authService.dart
@@ -1,4 +1,81 @@
-//This class is used only to authenticate the user
+//This class is used only to authenticate the user & save the current user
 //It utilizes the firebase_auth package
 //It may use methods from the dbService class to create a user in the database
 //Thus it may only use the firebase_auth package and dbService to manage users
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:tennisfundacionapp/services/dbService.dart';
+
+class AuthService {
+  final userStream = FirebaseAuth.instance.authStateChanges();
+
+  final user = FirebaseAuth.instance.currentUser;
+
+  signInEmailAndPass({required emailAddress, required password}) async {
+    //singin in and out of app using email and password
+    try {
+      await FirebaseAuth
+          .instance //should work but replace with final credential = await FirebaseAuth.instance if it does not
+          .signInWithEmailAndPassword(email: emailAddress, password: password);
+      if(await _isUserInDB(uid: user!.uid)){
+        await _addNewUserToDB();
+      }
+      
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        if (kDebugMode) {
+          print('No user found for that email.');
+        }
+      } else if (e.code == 'wrong-password') {
+        if (kDebugMode) {
+          print('Wrong password provided for that user.');
+        }
+      }
+    }
+  }
+
+  signOut() async {
+    await FirebaseAuth.instance.signOut();
+  }
+
+  Future<void> _addNewUserToDB() async {
+    try{
+
+      String? uid = user!.uid;
+      String? email = user!.email;
+      String? displayName = user!.displayName;
+      String? photoUrl = user!.photoURL;
+      String? type = 'donor';
+
+      photoUrl ??= '';
+
+      var newUser = {
+        'uid': uid,
+        'email': email,
+        'displayName': displayName,
+        'photoUrl': photoUrl,
+        'type': type,
+      };
+
+      await DBService().addEntryToDB(path: 'users', data: user!.uid, entry: newUser);
+    } catch (e) {
+      if(kDebugMode) {
+        print(e);
+      }
+    }
+  }
+
+  Future<bool> _isUserInDB({required String uid}) async {
+    try{
+      return DBService().isDataInDB(data: uid, path: 'users');
+    } catch (e) {
+      if(kDebugMode) {
+        print(e);
+        return false;
+      }
+    }
+    return false;
+  }
+
+}
--- lib/services/dbService.dart
+++ lib/services/dbService.dart
@@ -3,3 +3,108 @@
 //It will only have methods that are used to interact with the database
 //Methods such as "saveNewPhotoToDB" would not be part of this class as that is NOT a general database service
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
+
+class DBService{
+
+  FirebaseFirestore db = FirebaseFirestore.instance;
+
+
+  // will have 5 basic functions:
+  // add data to DB
+  // update data in DB
+  // delete data from DB
+  // fetch data from DB
+  // confirm existance of data in DB
+
+
+  /// Checks if a specific document exists in the database.
+///
+/// This function checks if a specific document exists in a specified path in the database.
+/// It requires a path and data as parameters. The data parameter is expected to be the name of the document in the database.
+///
+/// @param data The name of the document to check for in the database.
+/// @param path The path in the database where the document is expected to be.
+///
+/// @return A Future that completes with a boolean. Returns true if the document exists, false otherwise.
+Future<bool> isDataInDB({required data, required String path}) async {
+  try {
+    // Check if data is not null
+    if(data != null) {
+      // Fetch the document from the database
+      final doc = await getFromDB(path: path, data: data);
+      
+      // If the document exists, return true
+      if(doc != null) {
+        return true;
+      }
+    } else {
+      // If data is null, return false
+      return false;
+    }
+  } catch (e) {
+    // If an error occurs, print the error if in debug mode and return false
+    if(kDebugMode) {
+      print(e);
+    }
+  }
+  // If data is not found or an error occurs, return false
+  return false;
+}
+
+/// Fetches data from the database.
+///
+/// This function retrieves data from a specified path in the database.
+/// It requires a path and data as parameters.
+///
+/// @param path The path in the database from where data is to be retrieved.
+/// @param data The specific document to be retrieved from the database.
+///
+/// @return A Future that completes with a Map containing the data if it is found,
+/// or null if an error occurs or the data is not found.
+Future<Map<String, dynamic>?> getFromDB({required String path, required String data}) async {
+  try {
+    // Fetch the document from the database
+    final doc = await db.collection(path).doc(data).get();
+    
+    // Return the data of the document
+    return doc.data();
+  } catch (e) {
+    // If in debug mode, print the error
+    if (kDebugMode) {
+      print(e.toString());
+    }
+  }
+  // If an error occurs, return null
+  return null;
+}
+
+
+/// Adds an entry to the database.
+///
+/// This function adds a new document to a specified path in the database.
+/// It requires a path, an entry, and data as parameters. The data parameter is expected to be the name of the new document.
+///
+/// @param path The path in the database where the new document will be added.
+/// @param entry The data to be added to the new document. It should be a map where the keys are the field names and the values are the field values.
+/// @param data The name of the new document.
+///
+/// @return A Future that completes with a boolean. Returns true if the document is successfully added, false otherwise.
+Future<bool> addEntryToDB({required String path, required Map<String, dynamic> entry, required String data}) async {
+  try {
+    // Add the new document to the database
+    await db.collection(path).doc(data).set(entry);
+    // If the document is successfully added, return true
+    return true;
+  } catch (e) {
+    // If an error occurs, print the error if in debug mode
+    if (kDebugMode) {
+      print(e.toString());
+    }
+  }
+  // If an error occurs, return false
+  return false;
+}
+}
+
