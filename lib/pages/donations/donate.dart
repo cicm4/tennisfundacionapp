b49e6ac7c472452d9a43e9b2c985975f8abeb369
cicm4 <camilocorreall44@gmail.com>
2023-11-18 08:42:54 -0500

    something related to donate.dart

--- lib/pages/donations/donate.dart
+++ lib/pages/donations/donate.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pay/pay.dart';
 
 class Donate extends StatefulWidget {
   const Donate({super.key});
@@ -8,6 +9,22 @@ class Donate extends StatefulWidget {
 }
 
 class _DonateState extends State<Donate> {
+
+  late final PaymentConfiguration _googlePayConfigFuture;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void onGooglePayResult(paymentResult) {
+    debugPrint(paymentResult.toString());
+  }
+
+  void onApplePayResult(paymentResult) {
+    debugPrint(paymentResult.toString());
+  }
+  
   @override
   Widget build(BuildContext context) {
     return AppBar(
